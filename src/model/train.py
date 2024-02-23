23d246cb1f9adcfefaa5e4e763a978c4e0b4c892
Mitesh Rajan <mitesh.rajan@oxitec.com>
2024-02-23 15:39:43 +0000

    finish the first step

--- src/model/train.py
+++ src/model/train.py
@@ -7,12 +7,38 @@ import os
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
+import mlflow
 
 # define functions
-def main(args):
-    # TO DO: enable autologging
 
+def split_data(df):
+    # a funstion to split data
+    # last column = y
+    y = df.iloc[:, -1]
+    # all columns except last = X
+    X = df.iloc[:, :-1]
+    
+    # split data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    #print the length of the train and test data
+    print(f'Length of X_train: {len(X_train)}')
+    print("")
+    print(f'Length of X_test: {len(X_test)}')
+    print("")
+    print(f'Length of y_train: {len(y_train)}')
+    print("")
+    print(f'Length of y_test: {len(y_test)}')
+
+    return X_train, X_test, y_train, y_test
+
+def main(args):
+    # TO DO: enable autologging with MLflow
+    mlflow.autolog()
+    
+    print('Training data path', args.training_data)
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -53,7 +79,7 @@ def parse_args():
 
     # parse args
     args = parser.parse_args()
-
+    # print(args)
     # return args
     return args
 
